3c081f59c13b7fab0eb8bcfc5ce2fd3fb7eba3f6
Gurkan <ghasankaraman@users.noreply.github.com>
2020-11-08 13:07:06 +0300

    Matrix operations classes initialized

--- elementborne/Assets/Artificial_Intelligence/Matrix.cs
+++ elementborne/Assets/Artificial_Intelligence/Matrix.cs
@@ -5,20 +5,51 @@ public class Matrix
 {
     private double[,] matrix = null;
 
+    public int Row { get { return matrix.GetLength(0); } }
+    public int Column { get { return matrix.GetLength(1); } }
+
     public Matrix(double[,] matrix)
     {
         this.matrix = matrix;
     }
 
-    // Start is called before the first frame update
-    void Start()
+    public static Matrix Random(int row, int column)
     {
-        
+        double[,] matrix = new double[row, column];
+
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < column; j++)
+            {
+                matrix[i, j] = UnityEngine.Random.Range(0, 1f);
+            }
+        }
+
+        return new Matrix(matrix);
     }
 
-    // Update is called once per frame
-    void Update()
+    public static Matrix operator +(Matrix matrix1, Matrix matrix2) 
     {
-        
+        return new Matrix(Add(matrix1.matrix, matrix2.matrix));
+    }
+
+    private static double[,] Add(double[,] matrix1, double[,] matrix2)
+    {
+        return null;
+    }
+
+    private static double[,] Dot(double[,] matrix1, double[,] matrix2)
+    {
+        return null;
+    }
+
+    private static double[,] ElementWise(double[,] matrix1, double[,] matrix2)
+    {
+        return null;
+    }
+
+    private double[,] function(double[,] matrix1, double[,] matrix2)
+    {
+        return null;
     }
 }
