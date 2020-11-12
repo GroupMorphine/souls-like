1c24bc411a44d63ad6e1de117d1e4cd64991d326
Gurkan <ghasankaraman@users.noreply.github.com>
2020-11-12 11:26:55 +0300

    NN methods initialized

--- elementborne/Assets/Artificial_Intelligence/NeuralNetwork.cs
+++ elementborne/Assets/Artificial_Intelligence/NeuralNetwork.cs
@@ -1,18 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
-public class NeuralNetwork : MonoBehaviour
+public class NeuralNetwork
 {
-    // Start is called before the first frame update
-    void Start()
+    private List<Matrix> layers = new List<Matrix>();
+
+    public NeuralNetwork(params int[] layers)
     {
-        
+        for (int i = 1; i < layers.Length; i++)
+        {
+            Matrix matrix = Matrix.Random(layers[i], layers[i - 1]);
+            this.layers.Add(matrix);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private double ReLu(double x)
     {
-        
+        if (x < 0)
+            return 0;
+        return x;
+    }
+
+    private double Sigmoid(double x)
+    {
+        return 1 / (1 + System.Math.Exp(-x));
+    }
+
+    private Matrix Predict(double[,] inputs)
+    {
+        Matrix input = new Matrix(inputs);
+        Matrix z = null;
+        for (int i = 0; i < layers.Count - 1; i++)
+        {
+            z = input * layers[i];
+            z.function(ReLu);
+
+            input = z;
+        }
+
+        z = input * layers[layers.Count - 1];
+        z.function(Sigmoid);
+
+        return z;
     }
 }
