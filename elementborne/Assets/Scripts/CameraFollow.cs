f7039692f1416ed17ab2a4bc4eb1b79609b771a2
alicancv <alicansucu@outlook.com>
2020-12-23 13:51:34 +0300

    Olla artık

---
+++ elementborne/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    Transform target;
+    [SerializeField]
+    private float smoothness;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, new Vector3(target.position.x, target.position.y, target.position.z - 10), smoothness * Time.deltaTime);
+    }
+}
---
+++ elementborne/Assets/Scripts/MagicController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicController : MonoBehaviour
+{
+    public string magicName;
+    public float damage, speed, lifeTime, range, attackRate;
+    private bool isPickable = true;
+    public bool onAir;
+    public bool IsPickable
+    {
+        get { return isPickable; }
+        set { isPickable = value; }
+    }
+    public bool ActivateOnAir(bool isGrounded)
+    {
+        return (onAir || isGrounded);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+}
