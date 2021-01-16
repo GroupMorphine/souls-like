f9044173af63a110d283d11a8e75cb300a46789a
alicancv <alicansucu@outlook.com>
2021-01-16 15:32:37 +0300

    Environment Deneme

---
+++ elementborne/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    Transform target;
+    [SerializeField]
+    private float smoothness;
+    Vector3 offset;
+    void Awake()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        offset = transform.position - target.position;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, target.position + offset, smoothness * Time.deltaTime);
+    }
+}
