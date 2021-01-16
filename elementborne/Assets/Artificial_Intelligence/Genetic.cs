bfddd56f4a6dbdab93866f0730b533a4de887be0
Gurkan <ghasankaraman@users.noreply.github.com>
2021-01-16 22:18:13 +0300

    added copy func for nn class and initialized genetic class

--- elementborne/Assets/Artificial_Intelligence/Genetic.cs
+++ elementborne/Assets/Artificial_Intelligence/Genetic.cs
@@ -1,18 +1,53 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
-public class Genetic : MonoBehaviour
+public class Genetic
 {
-    // Start is called before the first frame update
-    void Start()
+    NeuralNetwork nn1;
+    NeuralNetwork nn2;
+
+    public Genetic(NeuralNetwork neural1, NeuralNetwork neural2)
     {
-        
+        if (neural1.weights.Count == neural2.weights.Count)
+        {
+            for (int i = 0; i < neural1.weights.Count; i++)
+            {
+                if (neural1.weights[i].Row == neural2.weights[i].Row &&
+                    neural2.weights[i].Column == neural2.weights[i].Column)
+                {
+                    this.nn1 = neural1;
+                    this.nn2 = neural2;
+                }
+                else 
+                {
+                    throw new Exception("The sizes are not same!");
+                }
+            }
+        }
+        else
+        {
+            throw new Exception("The sizes are not same!");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public NeuralNetwork CrossOver()
     {
-        
+        NeuralNetwork network = nn1.Copy();
+        for (int i = 0; i < network.weights.Count; i++)
+        {
+            for (int j = 0; j < network.weights[i].Row; j++)
+            {
+                for (int k = 0; k < network.weights[i].Column; k++)
+                {
+                    if (true)
+                    {
+
+                    }
+                }
+            }
+        }
+
+        return network;
     }
 }
--- elementborne/Assets/Artificial_Intelligence/NeuralNetwork.cs
+++ elementborne/Assets/Artificial_Intelligence/NeuralNetwork.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 
 public class NeuralNetwork
 {
-    private List<Matrix> layers = new List<Matrix>();
-
-    public NeuralNetwork(params int[] layers)
+    public List<Matrix> weights = new List<Matrix>();
+    private int[] layerSizes;
+    public NeuralNetwork(params int[] layerSizes)
     {
-        for (int i = 1; i < layers.Length; i++)
+        this.layerSizes = layerSizes;
+        for (int i = 1; i < layerSizes.Length; i++)
         {
-            Matrix matrix = Matrix.Random(layers[i], layers[i - 1]);
-            this.layers.Add(matrix);
+            Matrix matrix = Matrix.Random(layerSizes[i], layerSizes[i - 1]);
+            this.weights.Add(matrix);
         }
     }
 
@@ -26,21 +27,48 @@ public class NeuralNetwork
         return 1 / (1 + System.Math.Exp(-x));
     }
 
-    private Matrix Predict(double[,] inputs)
+    public Matrix Predict(double[,] inputs)
     {
         Matrix input = new Matrix(inputs);
         Matrix z = null;
-        for (int i = 0; i < layers.Count - 1; i++)
+        for (int i = 0; i < weights.Count - 1; i++)
         {
-            z = input * layers[i];
+            z = input * weights[i];
             z.function(ReLu);
 
             input = z;
         }
 
-        z = input * layers[layers.Count - 1];
+        z = input * weights[weights.Count - 1];
         z.function(Sigmoid);
 
         return z;
+    }
+
+    public NeuralNetwork Copy() 
+    {
+        List<Matrix> copy_layers = new List<Matrix>();
+
+        for (int i = 0; i < layerSizes.Length; i++)
+        {
+            Matrix matrix = Matrix.Random(layerSizes[i], layerSizes[i - 1]);
+            copy_layers.Add(matrix);
+        }
+
+        for (int i = 0; i < copy_layers.Count; i++)
+        {
+            for (int j = 0; j < copy_layers[i].Row; j++)
+            {
+                for (int k = 0; k < copy_layers[i].Column; k++)
+                {
+                    copy_layers[i][j, k] = weights[i][j, k];
+                }
+            }
+        }
+
+        NeuralNetwork copy_nn = new NeuralNetwork(layerSizes);
+        copy_nn.weights = copy_layers;
+
+        return copy_nn;
     }
 }
